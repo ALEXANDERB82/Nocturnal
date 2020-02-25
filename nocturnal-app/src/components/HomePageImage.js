9f08f8fb5e54ca9c9b16060d04742ea16b8198aa
ALEXANDERB82 <butlera@allegheny.edu>
2020-02-25 16:42:01 -0500

    package instalation

--- nocturnal-app/src/components/HomePageImage.js
+++ nocturnal-app/src/components/HomePageImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HomepageImage() {
   const url = 'https://cdn.filestackcontent.com/XYrHCaFGRSaq0EPKY1S6';
   return (
-    <img src={url} style={{width: 650}} alt='Image of Golden Gate Bridge' />
+    <img src={url} style={{width: 650}} alt='Golden Gate Bridge' />
   );
 }
 
---
+++ nocturnal-app/src/pages/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import HomepageImage from '../components/HomePageImage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomepageImage />
+        <p>
+          Hello whaats poppin
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
