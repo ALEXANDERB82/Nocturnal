640d9502e5848caee706352ea82a23e220aa0a28
ALEXANDERB82 <butlera@allegheny.edu>
2020-05-23 11:08:13 -0400

    reinstall missing packages

--- nocturnal-app/src/components/Library.js
+++ nocturnal-app/src/components/Library.js
@@ -1,6 +1,12 @@
 import React, { Fragment } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 
+var contentful = require('contentful')
+var client = contentful.createClient({
+  space: '4odwu2gvips4',
+  accessToken: 'TwJNF9D-6KM3U0zLCvLhTVUIedxF7vpPWGoJNThM3JE'
+})
+
 const Library = () => {
   const { loading, user } = useAuth0();
 
